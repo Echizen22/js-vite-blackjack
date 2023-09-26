0623aaa6258597ad61450c4fd86a82bfeccf57fc
Erixon Arce <erixon.arce.justiniano@gmail.com>
2023-09-26 18:50:55 +0200

    First Commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,14 @@
+
+/**
+ * Esta función nos permite pedir una carta del deck
+ * @param {Array<String>} deck 
+ * @returns {String} Retorna la carta del deck
+ */
+export const pedirCarta = ( deck ) => {
+
+    if ( !deck || deck.length === 0 ) 
+        throw 'No hay cartas en el deck';
+
+    const carta = deck.pop();
+    return carta;
+}
